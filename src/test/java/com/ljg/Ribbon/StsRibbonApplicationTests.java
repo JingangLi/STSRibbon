e2c0c646d8941ea35992ef32dd6b630f27f7eef7
JingangLi <ljg_emails@sina.com>
2017-12-30 21:36:10 +0100

    Add STS Ribbon code. Build ok.

---
+++ src/test/java/com/ljg/Ribbon/StsRibbonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ljg.Ribbon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StsRibbonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
